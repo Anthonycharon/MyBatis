bb35c90d744f739ae23d8ddafcb929d22bb49eae
tt <1983878557@qq.com>
2022-12-13 14:58:35 +0800

    hot-fix test

--- mybatis-pro001/src/test/java/com/doux/test/MyTest.java
+++ mybatis-pro001/src/test/java/com/doux/test/MyTest.java
@@ -35,5 +35,6 @@ public class MyTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("merge");
+        System.out.println("hot-fix test");
     }
 }
